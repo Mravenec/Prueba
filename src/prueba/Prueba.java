9a1d6c2be9aae7527f5378ef08ff754d10b4afdf
Mravenec <Mravenec@DESKTOP-0BR1UC1>
2017-10-15 17:35:31 -0600

    Cambie linea 20

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -17,7 +17,7 @@ public class Prueba {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
-        System.out.println("All good?");
+        System.out.println("Pablo");
     }
     
 }
