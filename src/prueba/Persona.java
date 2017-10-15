c4b963fffb643c5db877eb46e5e5cc882daef278
Pmora0813 <pablomora25@gmail.com>
2017-10-15 17:59:53 -0600

    Class Persona

---
+++ src/prueba/Persona.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+/**
+ *
+ * @author pmora_000
+ */
+public class Persona {
+    private String nombre;
+    
+    
+    public Persona(){
+        this.nombre = "K";
+    }
+    
+}
